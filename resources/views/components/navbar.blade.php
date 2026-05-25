e5951573a1e168411bd7fc24910137a1256a3ae4
AhmadRidhoHadaffi <ahmadridhohadaffi23@gmail.com>
2026-05-25 22:39:02 +0700

    refactor: memperbarui struktur navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,82 +1,37 @@
-<nav class="navbar navbar-expand-lg navbar-alpha fixed-top">
+{{-- resources/views/components/navbar.blade.php --}}
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNavbar">
     <div class="container">
-
-        {{-- logo kiri --}}
-        <a class="navbar-brand d-flex align-items-center p-0 m-0"
-           href="#beranda">
-
-            <img src="{{ asset('images/landing/logo.jpex`g') }}"
-                 alt="Alpha Event"
-                 class="logo-alpha">
+        <a class="navbar-brand fw-bold fs-4" href="#">
+            <span class="text-white">ALPHA</span><span class="text-accent">.</span><span class="text-white">COM</span>
         </a>
 
-        {{-- mobile --}}
-        <button class="navbar-toggler border-0 shadow-none"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarMenu">
-
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse"
-             id="navbarMenu">
-
-            {{-- tengah --}}
-            <ul class="navbar-nav mx-auto nav-center">
-
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto gap-lg-2">
                 <li class="nav-item">
-                    <a class="nav-link nav-alpha-link"
-                       href="#beranda">
-                        Beranda
-                    </a>
+                    <a class="nav-link" href="#beranda">Beranda</a>
                 </li>
-
                 <li class="nav-item">
-                    <a class="nav-link nav-alpha-link"
-                       href="#tentang">
-                        Tentang
-                    </a>
+                    <a class="nav-link" href="#layanan">Layanan</a>
                 </li>
-
                 <li class="nav-item">
-                    <a class="nav-link nav-alpha-link"
-                       href="#layanan">
-                        Layanan
-                    </a>
+                    <a class="nav-link" href="#tim">Tim</a>
                 </li>
-
                 <li class="nav-item">
-                    <a class="nav-link nav-alpha-link"
-                       href="#portfolio">
-                        Portofolio
-                    </a>
+                    <a class="nav-link" href="#portofolio">Portofolio</a>
                 </li>
-
                 <li class="nav-item">
-                    <a class="nav-link nav-alpha-link"
-                       href="#tim">
-                        Tim
-                    </a>
+                    <a class="nav-link" href="#tentang">Tentang</a>
                 </li>
-
             </ul>
 
-            {{-- kanan --}}
-            <div class="d-flex align-items-center nav-right">
-
-                <a href="/login"
-                   class="login-alpha">
-                    Masuk
-                </a>
-
-                <a href="#contact"
-                   class="btn btn-alpha-navbar">
-                    Mulai Sekarang
-                </a>
-
+            <div class="d-flex gap-2 mt-3 mt-lg-0">
+                <a href="#" class="btn btn-outline-light btn-sm px-3">Masuk</a>
+                <a href="#kontak" class="btn btn-accent btn-sm px-3">Hubungi Kami</a>
             </div>
-
         </div>
     </div>
 </nav>
